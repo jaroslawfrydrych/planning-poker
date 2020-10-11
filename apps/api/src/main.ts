85d544ac937be64826edf46ab1bf03064e96d6b6
Jarosław Frydrych <jarek.frydrych@gmail.com>
2020-10-12 01:56:23 +0200

    frontend redirect

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,12 +7,14 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { NotFoundExceptionFilter } from './frontend-redirect';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+  app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
