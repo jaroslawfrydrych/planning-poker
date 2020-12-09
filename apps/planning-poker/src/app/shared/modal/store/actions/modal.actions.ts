f329bd3c72771e0a79a96584263c69c9f6a5a37d
Jarosław Frydrych <jarek.frydrych@gmail.com>
2020-12-09 21:46:25 +0100

    visual updates

---
+++ apps/planning-poker/src/app/shared/modal/store/actions/modal.actions.ts
@@ -0,0 +1,9 @@
+export namespace ModalActions {
+  export class Show {
+    static readonly type = '[ModalActions] Show modal';
+  }
+
+  export class Close {
+    static readonly type = '[ModalActions] Close modal';
+  }
+}
--- apps/planning-poker/src/app/shared/modal/store/states/modal.state.ts
+++ apps/planning-poker/src/app/shared/modal/store/states/modal.state.ts
@@ -1,3 +1,10 @@
+import { State } from '@ngxs/store';
+
+import { ModalModel } from '../model/modal.model';
+
+@State<ModalModel>({
+  name: 'modal'
+})
 export class ModalState {
 
 }
